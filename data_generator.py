c76acd64b5a969586c668b45324c5fe175c45c9b
Rana <sahinra222@gmail.com>
2024-01-15 16:21:32 +0100

    more generators added

--- data_generator.py
+++ data_generator.py
@@ -1,6 +1,7 @@
 import random
 import string
 import datetime
+import json
 
 
 def generate_custom_id(prefix):
@@ -38,8 +39,51 @@ def generate_patient_data():
 
 
 def generate_practitioner_data():
-    pass
+    practitioner_json = {
+        "resourceType": "Practitioner",
+        "id": generate_custom_id("Practitioner"),
+        "address": address_generator(),
+        "qualification": [{
+            "code": {
+                "coding": [{
+                    "system": "http://terminology.hl7.org/CodeSystem/v2-0360/2.7",
+                    "code": "DM",
+                    "display": "Doctor of Medicine"
+                }],
+                "text": "Doctor of Medicine"
+            },
+            "issuer": {
+                "display": "Example University"
+            }
+        }]
+    }
+    return practitioner_json
 
 
 def generate_appointment_data():
-    pass
+    appointment_json = {
+      "resourceType": "Appointment",
+      "id": generate_custom_id("Appointment"),
+      "status": "booked",
+      "class": "ambulatory",
+      "description": "Discussion on the results of your recent MRI",
+      "start": "2013-12-10 09:00:00",
+      "end": "2013-12-10 11:00:00",
+      "created": "2013-10-10"
+    }
+    return appointment_json
+
+
+def generate_data(resource_type, data_count):
+    output_data = None
+    if resource_type == 'Patient':
+        generator_function = generate_patient_data
+    elif resource_type == 'Practitioner':
+        generator_function = generate_practitioner_data
+    elif resource_type == 'Appointment':
+        generator_function = generate_appointment_data
+
+    generated_data = generator_function()
+    if output_data:
+        with open("output.txt", 'w') as file:
+            json.dump(generated_data, file, indent=2)
